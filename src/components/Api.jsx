1248e7b07ac2d400da91c7f450b6bb43cf97f1b3
Kolade Faith <koladefaith1000@gmail.com>
2025-07-07 14:03:09 +0100

    Refactor Api component structure and update CSS styles for improved layout

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 import style from '../css/Api.module.css'
 
 const Api = () => {
+    const [quote, setquote] = useState()
+    const getQuote = () => {
+        let url = 
+    }
   return (
     <>
-        <h1 className={style.quoteHeading}>Quote of the Day</h1>
+      <h1 className={style.quoteHeading}>Quote of the Day</h1>
       <div className={style.container}>
         <div class={style.quotebox} id="quote-box">
-          <p id="quote" className="hidden">
-            Click the button to get a quote
-          </p>
-          <h3 id="author" className="hidden">
-            — Unknown
-          </h3>
+          <p className={style.quote}>Click the button to get a quote</p>
+          <h3 className={style.author}>— Unknown</h3>
         </div>
-        <button id="new-quote">Get Quote</button>
+        <button onClick={getQuote} className={style.newquote}>Get Quote</button>
       </div>
     </>
   );
